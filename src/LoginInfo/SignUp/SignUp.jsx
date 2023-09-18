6a325944cdc81155ab82646b803ddaeca2f3d225
Abid191 <abid15-12563@diu.edu.bd>
2023-09-18 11:46:55 +0600

    usersEmail post

--- src/LoginInfo/SignUp/SignUp.jsx
+++ src/LoginInfo/SignUp/SignUp.jsx
@@ -1,73 +1,89 @@
 import React, { useContext } from 'react';
 import { FaFacebook, FaGoogle } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const SignUp = () => {
 
-    const {createUser,googleLogIn} = useContext(AuthContext)
+    const { createUser, googleLogIn } = useContext(AuthContext)
 
-    const handleGoogleLogin = () =>{
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const from = location.state?.from?.pathname || "/"
+
+    const handleGoogleLogin = () => {
+
         googleLogIn()
-        .then(result => {    
-            alert('Login SuccessFully')
+            .then(result => {
+                const LoggedUser = result.user
+                const User = { name: LoggedUser.displayName, email: LoggedUser.email }
+                navigate(from, { replace: true });
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(User)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.insertedId) {
+                            alert('Login SuccessFully')
+                            
+                        }
 
-                const user = result.user
-                console.log(user)
+                    })
+
+
+                console.log(LoggedUser)
             })
-            .catch((error) =>{
+            .catch((error) => {
                 console.log(error)
             });
     }
 
-    const handleSignUp = event =>{
-
+    const handleSignUp = event => {
 
         event.preventDefault()
- 
-        const form = event.target 
-        const name = form.name.value 
-        const email = form.email.value 
-        const password = form.password.value 
-        const confirm = form.confirm.value 
- 
-        console.log(name,email,password,confirm)
 
-        const User = {name,email}
-        
+        const form = event.target
+        const name = form.name.value
+        const email = form.email.value
+        const password = form.password.value
+        const confirm = form.confirm.value
+
+        console.log(name, email, password, confirm)
+        const User = { name, email }
+        navigate(from, { replace: true });
         form.reset()
-        createUser(email,password) 
-        .then(result => {
-            alert('Registration SuccessFully')
+        createUser(email, password)
+            .then(result => {
 
-            const user = result.user
-            console.log(user)
-        })
-        .catch((error) => {
-            console.log(error)
-        });
-         
-        //  fetch('http://localhost:5000/users',{
-        //   method: 'POST',
-        //   headers:{
-        //   'content-type' : 'application/json'
-        //   },
-        //   body: JSON.stringify(User)
-        //  })
-        //  .then(res =>res.json())
-        //  .then(data => {
-        //      console.log(data)
- 
-        //  })
- 
-         // Signed in 
-         
-         // ...
-      
-       
-        
-     }
- 
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(User)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.insertedId) {
+                            alert('SignUp SuccessFully')
+                            
+                        }
+
+                    })
+                const user = result.user
+                console.log(user)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+
+    }
+
     return (
         <div className="hero min-h-screen  bg-base-200">
             <div className="hero-content flex-col ">
@@ -76,7 +92,7 @@ const SignUp = () => {
                 </div>
                 <div className="card  flex-shrink-0 w-full mt-5 shadow-2xl bg-base-100">
                     <form onSubmit={handleSignUp} className="card-body w-96">
-                        
+
                         <div className="form-control">
 
                             <input type="text" name='name' required placeholder="Name" className="input input-bordered" />
@@ -108,20 +124,20 @@ const SignUp = () => {
                     </form>
 
 
-                    <div className='mb-10 p-2 mx-10'> 
-                    <div className='text-center'>
-                        <h2>Connect with your Accounts</h2>
-                        <div className="divider"></div> 
-                    </div>
+                    <div className='mb-10 p-2 mx-10'>
+                        <div className='text-center'>
+                            <h2>Connect with your Accounts</h2>
+                            <div className="divider"></div>
+                        </div>
                         <div className='flex justify-center mx-auto gap-5 mt-10'>
                             <Link className=' '>
                                 <h1 className='text-2xl'> <FaFacebook /> </h1>
                             </Link>
-                            <Link className='  '> 
+                            <Link className='  '>
                                 <button className='' onClick={handleGoogleLogin} >
                                     <h1 className='text-2xl'> <FaGoogle /> </h1>
                                 </button>
-                                
+
                             </Link>
                         </div>
                     </div>
